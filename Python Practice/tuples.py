94859a05e7a1b5b73785327b02a7f8ac853f3f82
Azeez Sherif <azeezsheriff4321@gmail.com>
2026-09-11 21:02:47 +0100

    Initial commit

---
+++ Python Practice/tuples.py
@@ -0,0 +1,13 @@
+#DATA STRUCTURES
+#TUPLE DATA STRUCTURES
+
+
+
+coordinateX = 10.0
+coordinateY = 20.0
+
+coordinate = (10.0, 20.0)
+
+print(coordinate[1]) #This is a tuple and it is immutable
+
+#Sequence can be changed or modified (mutable) while Tuple cannot be changed or modified (Immutable)
